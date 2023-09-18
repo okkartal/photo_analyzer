5f86bb6f105b50c34a612c00f7dc89d441889274
okkartal <okadirkartal@gmail.com>
2023-09-18 04:03:04 +0400

    first commit

---
+++ Photos.Analyzer.Service/ComputerVisionAnalyzerService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Extensions.Configuration;
+using Photos.AnalyzerService.Abstractions;
+
+namespace Photos.AnalyzerService;
+public class ComputerVisionAnalyzerService : IAnalyzerService
+{
+
+    private readonly ComputerVisionClient client;
+    public ComputerVisionAnalyzerService(IConfiguration configuration)
+    {
+        var visionKey = configuration["VisionKey"];
+        var visionEndpoint = configuration["VisionEndpoint"];
+
+        client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(visionKey))
+        {
+            Endpoint = visionEndpoint
+        };
+    }
+
+    public async Task<dynamic> AnalyzeAsync(byte[] image)
+    {
+        using var ms = new MemoryStream(image);
+        var imageAnalysis = await client.AnalyzeImageInStreamAsync(ms);
+        var result = new
+        {
+            metadata = new
+            {
+                width = imageAnalysis.Metadata.Width,
+                height = imageAnalysis.Metadata.Height,
+                format = imageAnalysis.Metadata.Format
+            },
+            categories = imageAnalysis.Categories.Select(c => c.Name).ToArray()
+        };
+        return result;
+    }
+}
